7b010290bae5f3374819c47903dd92ce6297441e
BaconScripter <125394392+thanhdat4461@users.noreply.github.com>
2024-06-10 22:04:03 +0700

    Update No Name UI.lua

--- No Name UI.lua
+++ No Name UI.lua
@@ -346,7 +346,7 @@ function createButton(option, parent)
         library.flags[option.flag] = true
 	tweenService:Create(round, TweenInfo.new(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageColor3 = Color3.fromRGB(255, 65, 65)}):Play()
 	option.callback()
-        wait(1)
+        wait(0.5)
 	tweenService:Create(round, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageColor3 = Color3.fromRGB(60, 60, 60)}):Play()
     end)
 end
